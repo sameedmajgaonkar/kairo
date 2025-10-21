395ff3aaa91b37757fb3cd60089a84d2e884a23b
Sameed Majgaonkar <sameed1718@gmail.com>
2025-10-21 15:13:33 +0530

    Built an e2b template and run code in sandbox

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,11 +1,19 @@
 import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 import { inngest } from "./client";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+    // Create a sandbox using prebuilt template
+
+    const sandboxId = await step.run("get-sandboxId", async () => {
+      const sandbox = await Sandbox.create("codex-nextjs-test-1718");
+      return sandbox.sandboxId;
+    });
+
     const model = new ChatGoogleGenerativeAI({
       model: "gemini-2.5-flash",
       temperature: 0,
@@ -16,6 +24,11 @@ export const helloWorld = inngest.createFunction(
     ];
     const response = await model.invoke(messages);
 
-    return { message: response.content };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+    return { message: response.content, sandboxUrl };
   }
 );
