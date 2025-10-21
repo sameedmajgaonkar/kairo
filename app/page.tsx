0795f8c3b3414f363becbf8e4086064185b8b8f7
Sameed Majgaonkar <sameed1718@gmail.com>
2025-10-21 10:46:20 +0530

    Setup TRPC

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
 import { Button } from "@/components/ui/button";
+import { caller, getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
 
-export default function Home() {
-  return <Button variant="outline">Codex</Button>;
+export default async function Home() {
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: "Sameed" }));
+  const data = await caller.hello({ text: "Sameedd" });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
+  );
 }
---
+++ trpc/init.ts
@@ -0,0 +1,15 @@
+import { initTRPC } from "@trpc/server";
+import { cache } from "react";
+import superjson from "superjson";
+
+export const createTRPCContext = cache(async () => {
+  return { user: "Sameed" };
+});
+
+const t = initTRPC.create({
+  transformer: superjson,
+});
+
+export const createTRPCRouter = t.router;
+export const createCallerFactory = t.createCallerFactory;
+export const baseProcedure = t.procedure;
